ca2fb9b5aff57622d0da5fc62eedff241ce84dbf
Victor <110458160+roirobertooo@users.noreply.github.com>
2024-10-19 01:29:34 +0800

    for charts and supabbase client

--- app/(reports)/report-1/page.tsx
+++ app/(reports)/report-1/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import {Charts} from "./overview_components";
+import {supabase} from "@/utils/supabase/client";
 
-function Report1_1() {
-  return (
-    <div className="py-16 border-b">
-      <h1>Report 1_1</h1>
-    </div>
-  );
+async function Report1_1() {
+    const {data, error} = await supabase.rpc('get_total_games');
+
+    return (
+        <div className="py-16 border-b">
+            <h1 className="font-medium text-xl">Report 1_1</h1>
+            {data}
+            <Charts/>
+        </div>
+    );
 }
 
 function Report1_2() {
@@ -32,16 +38,16 @@ function Report1_4() {
   );
 }
 
-export default function Report1() {
-  return (
-    <div className="w-3/4 flex flex-col self-center p-3">
-      <div className="font-bold text-3xl">Overview</div>
-      <div className="container mx-auto">
-        <Report1_1/>
-        <Report1_2/>
-        <Report1_3/>
-        <Report1_4/>
-      </div>
-    </div>
-  );
+export default async function Report1() {
+    return (
+        <div className="w-3/4 flex flex-col self-center p-3">
+            <div className="font-bold text-3xl">Overview</div>
+            <div className="container mx-auto">
+                <Report1_1 />
+                <Report1_2/>
+                <Report1_3/>
+                <Report1_4/>
+            </div>
+        </div>
+    );
 }
